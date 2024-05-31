794de7ff047218db5d6036b497771cb885c7dbf9
Codentide <marcodelboccio77@gmail.com>
2024-05-31 18:41:21 -0500

    updating simple-card

--- src/components/cards/simple-card/simple-card.js
+++ src/components/cards/simple-card/simple-card.js
@@ -1,35 +1,35 @@
 import {
-  html,
-  mapComponentEvents,
-  updateVars,
-  registerTriggers,
-} from '../../../global/web-tools';
+	html,
+	mapComponentEvents,
+	updateVars,
+	registerTriggers,
+} from "../../../global/web-tools";
 
-import componentHtml from './simple-card.html';
-import componentStyle from './simple-card.css';
+import componentHtml from "./simple-card.html";
+import componentStyle from "./simple-card.css";
 
 export default class simpleCard extends HTMLElement {
-  constructor() {
-    super();
-    const template = html` <style>
-        ${componentStyle}
-      </style>
-      ${componentHtml}`;
-    this.attachShadow({ mode: 'open' });
-    this.shadowRoot.appendChild(template);
-  }
+	constructor() {
+		super();
+		const template = html` <style>
+				${componentStyle}
+			</style>
+			${componentHtml}`;
+		this.attachShadow({ mode: "open" });
+		this.shadowRoot.appendChild(template);
+	}
 
-  connectedCallback() {
-    mapComponentEvents(this);
-    updateVars(this);
-    registerTriggers(this, (event) => console.log(event));
-  }
+	connectedCallback() {
+		mapComponentEvents(this);
+		updateVars(this);
+		registerTriggers(this, (event) => console.log(event));
+	}
 
-  disconnectedCallback() {}
+	disconnectedCallback() {}
 
-  attributeChangedCallback(name, oldValue, newValue) {}
+	attributeChangedCallback(name, oldValue, newValue) {}
 
-  adoptedCallback() {}
+	adoptedCallback() {}
 }
 
-window.customElements.define('simple-card', simpleCard);
+window.customElements.define("simple-card", simpleCard);
