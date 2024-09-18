9367a2cc03f6c9afb1668263269881159f79799c
Codentide <marcodelboccio77@gmail.com>
2024-09-18 15:51:52 -0500

    Infra: Adding domainName

--- dwck-infra/lib/dwck-infra-stack.ts
+++ dwck-infra/lib/dwck-infra-stack.ts
@@ -15,7 +15,14 @@ export class DwckInfraStack extends cdk.Stack {
   }
 
   private setupWebapp() {
-    const webapp = new WebAppConstruct(this, 'dwckWebapp')
+    const webapp = new WebAppConstruct(this, 'dwckWebapp', {
+      domainName: 'dev-archy.com',
+      certArn:
+        'arn:aws:acm:us-east-1:905418338837:certificate/04a50b00-cb01-4ca2-b566-465c8c85c335',
+      // @ts-ignore
+      hostedZoneId: 'Z03499403A5GA5MZQP82V'
+    })
+
     webapp.addAssets('./webapps/landing-page')
     webapp.addAssets('../demos', 'demos')
     webapp.addAssets('../lib', 'lib')
